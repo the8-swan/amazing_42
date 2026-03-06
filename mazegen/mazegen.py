306aaeaa7a3e10b4bf59694587fce86d9ff09a49
the8-swan <hananebmd077@gmail.com>
2026-03-06 04:05:51 +0000

    pushed version

--- mazegen/mazegen.py
+++ mazegen/mazegen.py
@@ -220,7 +220,7 @@ class Maze:
         """Introduce random wall removals to create loops
         and make the maze imperfect."""
         if self.perfect is False:
-          
+
             unvisited: list[tuple[int, int]] = []
             visited: list[tuple[int, int]] = []
             dirs: list[str] = ['E', 'W', 'N', 'S']
@@ -241,7 +241,7 @@ class Maze:
                         visited.append((c, r))
                     else:
                         unvisited.append((c, r))
-          
+
             while wall > 0 and unvisited:
                 x: int
                 y: int
@@ -250,6 +250,7 @@ class Maze:
                 x, y = random.choice((unvisited))
                 unvisited.remove((x, y))
                 dict_42: dict[str, bool] = {}
+                count: int = 0
                 for d in dirs:
                     m_x, m_y, n_dir, m_dir = self.direction[d]
                     n_x, n_y = x + m_x, y + m_y
@@ -258,24 +259,33 @@ class Maze:
                             dict_42[n_dir] = False
                         else:
                             dict_42[n_dir] = True
+
                     else:
                         dict_42[n_dir] = False
+                    if not self.cells[y][x].walls[n_dir]:
+                        count += 1
 
                 random.shuffle(dirs)
-                found: bool = True
-                i: int = 0
-                while i < 4 and found:
-                    next_dir = dirs[i]
+                found2: bool = True
+                j: int = 0
+                while j < 4 and found2:
+                    next_dir = dirs[j]
                     m_x, m_y, n_dir, m_dir = self.direction[next_dir]
                     n_x, n_y = x + m_x, y + m_y
                     if 0 <= n_x < w and 0 <= n_y < h:
                         if (self.cells[n_y][n_x].walls[m_dir]
-                                and dict_42[next_dir]):
-                            self.cells[y][x].walls[n_dir] = False
-                            self.cells[n_y][n_x].walls[m_dir] = False
-                            wall -= 1
-                            found = False
-                    i += 1
+                                and dict_42[next_dir]
+                                and count <= 2):
+                            count2: int = 0
+                            for d in dirs:
+                                if not self.cells[n_y][n_x].walls[d]:
+                                    count2 += 1
+                            if count2 <= 2:
+                                self.cells[y][x].walls[n_dir] = False
+                                self.cells[n_y][n_x].walls[m_dir] = False
+                                wall -= 1
+                                found2 = False
+                    j += 1
 
     def wilson_algo(self) -> None:
         """Generate the maze using Wilson's algorithm with
